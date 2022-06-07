15cb6a36c332ff7b3fa0fe7aa793e8d66d51f985
Jeewoo Lee <leejeewoo@berkeley.edu>
2022-06-07 17:00:33 +0900

    deployed the contract to rinkeby using Hardhat modules

---
+++ hardhat.config.js
@@ -0,0 +1,58 @@
+require("dotenv").config()
+
+require("@nomiclabs/hardhat-etherscan")
+require("@nomiclabs/hardhat-waffle")
+require("hardhat-gas-reporter")
+require("solidity-coverage")
+require("hardhat-deploy")
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+const RINKEBY_URL = process.env.RINKEBY_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSACN_API_KEY = process.env.ETHERSACN_API_KEY
+
+console.log(RINKEBY_URL)
+
+module.exports = {
+    solidity: {
+        compilers: [{ version: "0.8.8" }, { version: "0.6.6" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        rinkeby: {
+            url: RINKEBY_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 4,
+            blockConfirmations: 6,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "MATIC",
+    },
+    etherscan: {
+        apiKey: {
+            rinkeby: ETHERSACN_API_KEY,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+            // 4: 1,
+        },
+        user: {
+            default: 1,
+        },
+    },
+}
